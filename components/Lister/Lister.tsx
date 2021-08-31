e8bc4f0680d46fe1cd8198a215d281f506dd4f7b
Lindsay Marean <lmarean@bensay.org>
2021-08-31 13:46:44 -0700

    a little more robust

--- components/Lister/Lister.tsx
+++ components/Lister/Lister.tsx
@@ -1,15 +1,12 @@
 export default function Lister(itemArray: Array<any>) {
-    if (typeof (itemArray) === "undefined") {
-        return (<ul></ul>);
-    }
-    else {
+    if (typeof (itemArray) !== "undefined" && itemArray.length && itemArray[0].id && itemArray[0].name) {
         const items = itemArray.map((item: any) =>
             <li key={item.id}>{item.name}</li>);
-
         return (
             < ul >
                 {items}
             </ul >
         );
     }
+    else return (<ul></ul>)
 };
