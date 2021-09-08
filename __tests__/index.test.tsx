15eb44c9622e6d6f6aa5085f0f91d70cda3b789f
Lindsay Marean <lmarean@bensay.org>
2021-09-08 10:45:16 -0700

    test for logged in session added

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -20,6 +20,8 @@ describe("Home", () => {
     expect(footer).toBeInTheDocument();
   });
 
+  // model for session tests: https://github.com/nextauthjs/next-auth/issues/775
+
   it("renders correctly when signed out", () => {
     (useSession as jest.Mock).mockReturnValueOnce([false, false]);
 
@@ -27,4 +29,23 @@ describe("Home", () => {
     render(<Home mealplans={arg} />);
     expect(screen.getByText("Not signed in"));
   });
+
+  it("renders correctly when signed in", () => {
+    (useSession as jest.Mock).mockReturnValueOnce([
+      {
+        user: {
+          email: null,
+          image: "http://www.bensay.org/gallery/v/grandcanyon/toppicks/1711.JPG",
+          name: "testuser"
+        },
+        accessToken: "123abc",
+        expires: "2022-10-08T16:50:26.863Z"
+      },
+      false,
+    ]);
+    const arg: MealPlan[] = [];
+    render(<Home mealplans={arg} />);
+    expect(screen.getByText("Signed in as testuser"));
+  });
+
 });
